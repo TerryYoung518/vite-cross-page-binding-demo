1d7e5e061ec917fabe4545bfde15084fe9995166
Krue1 <44221403+Krue1@users.noreply.github.com>
2023-05-12 17:24:09 +0800

    Utilize the singleton pattern to replace new-ing a BroadcastChannel each time. (#1)
    * feat: Utilize the singleton pattern to replace new-ing a BroadcastChannel each time.
    
    ---------
    
    Co-authored-by: lianghaobin1 <lhbin@cndatacom.com>

--- src/components/BindCast.ts
+++ src/components/BindCast.ts
@@ -1,18 +1,66 @@
-import { Ref, computed } from "vue"
+import { Ref, computed } from "vue";
 
-export function binding<T>(val: Ref<T>, id: string) {
-  const channel = new BroadcastChannel('cross_page_binding_' + id);
-  channel.onmessage = (messageEvent: MessageEvent<T>) => {
-    val.value = messageEvent.data
+interface MessageData {
+  fieldKey: string;
+  value: unknown;
+}
+
+class BroadcastChannelManager {
+  private static instance: BroadcastChannelManager;
+
+  private readonly channel: BroadcastChannel;
+
+  private readonly callbacks: Record<string, Function> = {};
+
+  private constructor() {
+    this.channel = new BroadcastChannel("cross_page_binding");
+    this.channel.onmessage = (event: MessageEvent<MessageData>) => {
+      this.callbacks[event.data.fieldKey]?.(event.data);
+    };
   }
-  channel.postMessage(val.value)
-  return computed<T>({
-    get: () => {
-      return val.value
-    },
-    set: (newVal: T) => {
-      val.value = newVal
-      channel.postMessage(newVal)
+
+  public static getInstance(): BroadcastChannelManager {
+    if (!BroadcastChannelManager.instance) {
+      BroadcastChannelManager.instance = new BroadcastChannelManager();
     }
-  })
+    return BroadcastChannelManager.instance;
+  }
+
+  public getMessage<MessageData>(
+    callback: (data: MessageData) => void,
+    fieldKey: string
+  ): void {
+    this.callbacks[fieldKey] = this.callbacks[fieldKey] || callback;
+  }
+
+  public postMessage<MessageData>(data: MessageData): void {
+    this.channel.postMessage(data);
+  }
+
+  public close(): void {
+    this.channel.close();
+  }
+}
+
+export function binding<T>(val: Ref<T>, fieldKey: string): Ref<T> {
+  const manager = BroadcastChannelManager.getInstance();
+
+  manager.getMessage<MessageData>((data) => {
+    if (data.fieldKey === fieldKey) {
+      val.value = data.value as T;
+    }
+  }, fieldKey);
+
+  return computed<T>({
+    get: () => val.value,
+    set: (value: T) => {
+      val.value = value;
+      manager.postMessage({ fieldKey, value });
+    },
+  });
+}
+
+export function closeBroadcastChannel(): void {
+  const manager = BroadcastChannelManager.getInstance();
+  manager.close();
 }
